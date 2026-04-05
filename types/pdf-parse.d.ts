1622797163f5bfc55a1963d18963a50a333feee9
mabro <mabro@users.noreply.github.com>
2026-04-05 15:15:20 -0500

    fix(types): add pdf-parse declaration

--- types/pdf-parse.d.ts
+++ types/pdf-parse.d.ts
@@ -1,1 +1,4 @@
-declare module 'pdf-parse/lib/pdf-parse.js';
+declare module 'pdf-parse' {
+  function pdfParse(dataBuffer: Buffer): Promise<{ text: string; numpages: number; info: any; metadata: any; version: string }>;
+  export = pdfParse;
+}

9c6fe06ac7dd807e4f824c3ca3f1e5804cc75233
Michael <mabrown040@gmail.com>
2026-05-11 17:28:00 -0500

    chore(i18n): remove ghost i18next dependencies and dead next-i18next.config.js

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es', 'fr', 'pt'],
-  },
-  localePath: './public/locales',
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-};
